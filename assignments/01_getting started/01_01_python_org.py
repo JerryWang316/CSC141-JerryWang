e17422ecf04907a6194a40d78872d8aeae6d7f4d
JerryWang316 <tianrui.wang001@albright.edu>
2025-09-05 00:09:19 +0800

    Add files via upload

---
+++ assignments/01_getting started/01_01_python_org.py
@@ -0,0 +1,8 @@
+#01_01_python_org
+#my first assignment
+
+#The biggest changes of python 3.13 is the new interactive interpreter
+
+#Error messages continue to improve, with tracebacks now highlighted in color by default
+
+#Several legacy standard library modules have now been removed following their deprecation in Python 3.11
