608f55b1441f2bfc6e65a66a50f9e88eab51875b
JerryWang316 <tianrui.wang001@albright.edu>
2025-09-06 00:04:40 +0800

    Add files via upload

---
+++ assignments/01_getting started/01_03_infinite_skills.py
@@ -0,0 +1,10 @@
+"""
+
+I want a mega power to make me good at coding skills.
+
+I want to be able to code in any programming language.
+
+I want to be able to learn new coding skills instantly.
+
+
+"""
