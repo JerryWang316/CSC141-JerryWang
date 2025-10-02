442d183c41686034c58e5371cfb19ece1298c014
JerryWang316 <tianrui.wang001@albright.edu>
2025-10-03 00:25:51 +0800

    Delete assignments/06_dictionaries/06_dictionaries directory

--- assignments/06_dictionaries/06_dictionaries/06_01_person.py
+++
@@ -1,6 +0,0 @@
-person = {'first_name': 'Tianrui','last_name': 'Wang','age': 20,'city': 'Hong Kong'}
-
-print(f"First name: {person['first_name']}")
-print(f"Last name: {person['last_name']}")
-print(f"Age: {person['age']}")
-print(f"City: {person['city']}")
--- assignments/06_dictionaries/06_dictionaries/06_02_favorite_numbers.py
+++
@@ -1,6 +0,0 @@
-favorite_numbers = {'Jerry': 6,'Seyeon': 5,'Sohyun': 8,'Marie': 9,'Marcello': 1}
-
-print("Favorite Numbers:")
-print("-" * 20)
-for name, number in favorite_numbers.items():
-    print(f"{name}: {number}")
